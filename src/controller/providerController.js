dc7db8bccfa0d0b8ab5d2402c193263f6abaea66
Nicolas Ferretti <nicolas_ferretti@mcafee.com>
2018-04-02 01:19:13 -0300

    Login service

--- src/controller/providerController.js
+++ src/controller/providerController.js
@@ -4,6 +4,20 @@ const showProviders = (providerId, callbackSuccess) => {
   dao.showProviders(providerId, callbackSuccess);
 };
 
+const showOrders = (callbackSuccess) => {
+  dao.showOrders(callbackSuccess);
+};
+
+const newOrder = (order) => {
+  const promise = (resolve, reject) => {
+    dao.newOrder(order)
+      .then(() => resolve(), err => reject(err));
+  };
+  return new Promise(promise);
+};
+
 module.exports = {
   showProviders,
+  newOrder,
+  showOrders,
 };
