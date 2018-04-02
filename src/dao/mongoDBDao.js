577aed0d3ccda9376aca71cf238eb049bb590d5a
Nicolas Ferretti <nicolas_ferretti@mcafee.com>
2018-04-01 23:34:25 -0300

    Login service

--- src/dao/mongoDBDao.js
+++ src/dao/mongoDBDao.js
@@ -62,9 +62,41 @@ function MongoDBDao() {
   };
 
   this.storeCustomer = function (customer) {
+    const promiseFind = new Promise((resolve, reject) => {
+      connection.collection('customers').count((err, res) => {
+        if (err) {
+          console.error(`Error:  ${err}`);
+          const error = {
+            code: 400,
+            message: 'Internal Server Error.',
+          };
+          return reject(error);
+        }
+        customer.id = parseInt(res) +1;
+        resolve(true);
+      });
+    }).then(() => {
+      return new Promise((resolve, reject) => {
+        connection.collection('customers').insertOne(customer, function (err) {
+          if (err) {
+            console.error(`Error:  ${err}`);
+            const error = {
+              code: 500,
+              message: 'Internal Server Error.',
+            };
+            return reject(error);
+          }
+          console.info('customer inserted');
+          resolve();
+        });
+      });
+    });
+    return promiseFind;
+  };
+
+  /*this.storeCustomer = function (customer) {
     const promise = (resolve, reject) => {
-      connection.collection('customers')
-        .insertOne(customer, function (err) {
+      connection.collection('customers').insertOne(customer, function (err) {
           if (err) {
             console.error(`Error:  ${err}`);
             const error = {
@@ -78,7 +110,7 @@ function MongoDBDao() {
         });
     };
     return new Promise(promise);
-  };
+  };*/
 
   this.modifyCustomer = function (customer) {
     return new Promise((resolve, reject) => {
@@ -115,6 +147,43 @@ function MongoDBDao() {
           resolve(res.deletedCount);
           console.info('Customer in process of delete');
         });
+    });
+  };
+
+  this.showProviders = (providerId, callback) => {
+    return new Promise((resolve, reject) => {
+      if (!providerId) {
+        connection.collection('providers')
+          .find({})
+          .toArray(function (err, result) {
+            if (err) {
+              console.error(`Error:  ${err}`);
+              const error = {
+                code: 400,
+                message: 'Internal Server Error.',
+              };
+              return reject(error);
+            }
+            callback(result);
+            resolve(true);
+          });
+      } else {
+        let query = { id: providerId };
+        connection.collection('providers')
+          .find(query)
+          .toArray(function (err, result) {
+            if (err) {
+              console.error(`Error:  ${err}`);
+              const error = {
+                code: 400,
+                message: 'Internal Server Error.',
+              };
+              return reject(error);
+            }
+            callback(result);
+            resolve(true);
+          });
+      }
     });
   };
 
